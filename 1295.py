517a7733aecd87a4bacdbbb70e6475650d802e51
Harry Monse <harry.monse1812@gmail.com>
2024-01-18 15:11:36 +0530

    1295

---
+++ 1295.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            if len(str(nums[i])) % 2 == 0:
+                count += 1
+        return count
+    
+
+solution_instance = Solution()
+
+result = solution_instance.findNumbers(nums = [12,345,2,6,7896])
+print(result)
+
+result = solution_instance.findNumbers(nums = [555,901,482,1771])
+print(result)
+
+
+
+
+
+
+
+
+        
