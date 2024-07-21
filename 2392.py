3b91f7140e23f33d8be98ef7d3c6abac0d4916f1
Harry Monse <harry.monse1812@gmail.com>
2024-07-21 06:32:22 +0530

    2392

---
+++ 2392.py
@@ -0,0 +1,45 @@
+from collections import defaultdict, deque
+from typing import List
+
+
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        def f(cond):
+            g = defaultdict(list)
+            indeg = [0] * (k + 1)
+            for a, b in cond:
+                g[a].append(b)
+                indeg[b] += 1
+            q = deque([i for i, v in enumerate(indeg[1:], 1) if v == 0])
+            res = []
+            while q:
+                for _ in range(len(q)):
+                    i = q.popleft()
+                    res.append(i)
+                    for j in g[i]:
+                        indeg[j] -= 1
+                        if indeg[j] == 0:
+                            q.append(j)
+            return None if len(res) != k else res
+
+        row = f(rowConditions)
+        col = f(colConditions)
+        if row is None or col is None:
+            return []
+        ans = [[0] * k for _ in range(k)]
+        m = [0] * (k + 1)
+        for i, v in enumerate(col):
+            m[v] = i
+        for i, v in enumerate(row):
+            ans[i][m[v]] = v
+        return ans
+    
+
+solution_instance = Solution()
+
+result = solution_instance.buildMatrix(k = 3, rowConditions = [[1,2],[3,2]], colConditions = [[2,1],[3,2]])
+print(result)
+
+result = solution_instance.buildMatrix(k = 3, rowConditions = [[1,2],[2,3],[3,1],[2,3]], colConditions = [[2,1]])
+print(result)
+
