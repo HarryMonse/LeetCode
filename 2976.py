8a564c139413e0cfdb647ced7bdb4cc7da0a2fe9
Harry Monse <harry.monse1812@gmail.com>
2024-07-27 16:39:11 +0530

    2976.

--- 2976.py
+++ 2976.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
         dp = [[float('inf')] * 26 for _ in range(26)]
@@ -12,5 +15,17 @@ class Solution:
                     dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
         res = sum(dp[ord(s) - ord('a')][ord(t) - ord('a')] for s, t in zip(source, target))
         return res if res != float('inf') else -1
+
+
+solution_instance = Solution()
+
+result = solution_instance.minimumCost(source = "abcd", target = "acbe", original = ["a","b","c","c","e","d"], changed = ["b","c","b","e","b","e"], cost = [2,5,5,1,2,20])
+print(result)
+
+result = solution_instance.minimumCost(source = "aaaa", target = "bbbb", original = ["a","c"], changed = ["c","b"], cost = [1,2])
+print(result)
+
+result = solution_instance.minimumCost(source = "abcd", target = "abce", original = ["a"], changed = ["e"], cost = [10000])
+print(result)
 
 
