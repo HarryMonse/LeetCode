fd3608e80d21fcd653adca79cfb370335e481e93
Harry Monse <harry.monse1812@gmail.com>
2024-07-29 17:58:49 +0530

    1395

---
+++ 1395.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        n = len(rating)
+        count = 0
+        
+        for j in range(n):
+            leftLess = leftGreater = rightLess = rightGreater = 0
+
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    leftLess += 1
+                elif rating[i] > rating[j]:
+                    leftGreater += 1
+            
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    rightLess += 1
+                elif rating[k] > rating[j]:
+                    rightGreater += 1
+            
+            count += leftLess * rightGreater + leftGreater * rightLess
+        
+        return count
+    
+
+solution_instance = Solution()
+
+result = solution_instance.numTeams(rating = [2,5,3,4,1])
+print(result)
+
+result = solution_instance.numTeams(rating = [2,1,3])
+print(result)
+
+result = solution_instance.numTeams(rating = [1,2,3,4])
+print(result)
+
