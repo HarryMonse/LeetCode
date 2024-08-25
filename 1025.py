55b231dba24054ca0db4673baf847e1507d708b3
Harry Monse <harry.monse1812@gmail.com>
2024-08-25 17:06:07 +0530

    1025

--- 1025.py
+++ 1025.py
@@ -4,3 +4,12 @@ class Solution:
             return True
         else:
             return False
+        
+
+solution_instance = Solution()
+
+result = solution_instance.divisorGame(n = 2)
+print(result)
+
+result = solution_instance.divisorGame(n = 3)
+print(result)
