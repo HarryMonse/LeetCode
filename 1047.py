10d7f715dacd4167d09dd2edbfaaa03f01ba844b
Harry Monse <harry.monse1812@gmail.com>
2024-08-27 16:45:47 +0530

    1047

---
+++ 1047.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        lis=[s[0]]
+        for i in range(1,len(s)):
+            if len(lis)==0 or lis[len(lis)-1]!=s[i]:
+                lis.append(s[i])
+            else:
+                lis=lis[:len(lis)-1]        
+        s="".join(lis)
+        return s    
+
+
+solution_instance = Solution()
+
+result = solution_instance.removeDuplicates(s = "abbaca")
+print(result)
+
+result = solution_instance.removeDuplicates(s = "azxxzy")
+print(result)
