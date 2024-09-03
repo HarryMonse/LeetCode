42609ef387e402163c61fc252ac3a14d5c29f434
Harry Monse <harry.monse1812@gmail.com>
2024-09-03 15:20:31 +0530

    1385.

--- 1385.py
+++ 1385.py
@@ -13,3 +13,15 @@ class Solution:
             if flag == 0:
                 cnt += 1
         return cnt
+    
+
+solution_instance = Solution()
+
+result = solution_instance.findTheDistanceValue(arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2)
+print(result)
+
+result = solution_instance.findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3)
+print(result)
+
+result = solution_instance.findTheDistanceValue(arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6)
+print(result)
