bb3508e079785619986ae9f18dd643dc9a3da655
Harry Monse <harry.monse1812@gmail.com>
2024-09-25 15:26:45 +0530

    1876

---
+++ 1876.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)-2):
+            if len(set(s[i:i+3])) == 3:
+                count += 1
+        return count
+    
+
+solution_instance = Solution()
+
+result = solution_instance.countGoodSubstrings(s = "xyzzaz")
+print(result)
+
+result = solution_instance.countGoodSubstrings(s = "aababcabc")
+print(result)
+
