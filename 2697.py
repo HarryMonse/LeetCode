d6f4157516bf31a6c2005920b2011b241e980d1e
Harry Monse <harry.monse1812@gmail.com>
2024-11-14 14:28:58 +0530

    2697

---
+++ 2697.py
@@ -0,0 +1,21 @@
+class Solution:
+    def makeSmallestPalindrome(self, s: str) -> str:
+        for i in range(int(len(s)/2)):
+            if s[i] != s[len(s)-i-1]:
+                if s[i] < s[len(s)-i-1]:
+                    s = s[:len(s)-i-1] + s[i] + s[len(s)-i:]
+                else:
+                    s = s[:i] + s[len(s)-i-1] + s[i+1:]
+        return s
+    
+
+solution_instance = Solution()
+
+result = solution_instance.makeSmallestPalindrome(s = "egcfe")
+print(result)
+
+result = solution_instance.makeSmallestPalindrome(s = "abcd")
+print(result)
+
+result = solution_instance.makeSmallestPalindrome(s = "seven")
+print(result)
