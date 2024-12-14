33be6dc010025ba8f4bc0eb9e673f3df0c6ee302
Harry Monse <harry.monse1812@gmail.com>
2024-12-14 16:11:51 +0530

    3330

---
+++ 3330.py
@@ -0,0 +1,20 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        n=len(word)
+        count=1
+        for i in range(n):
+            if i+1<n and word[i]==word[i+1]:
+                count+=1
+        return count
+    
+
+solution_instance = Solution()
+
+result = solution_instance.possibleStringCount(word = "abbcccc")
+print(result)
+
+result = solution_instance.possibleStringCount(word = "abcd")
+print(result)
+
+result = solution_instance.possibleStringCount(word = "aaaa")
+print(result)
