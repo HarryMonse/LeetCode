de53818a89b9aba0b3a78798f5d6a3572bb2d434
Harry Monse <harry.monse1812@gmail.com>
2024-12-29 16:59:26 +0530

    3300

---
+++ 3300.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        mini=float('inf')
+        for num in nums:
+            r=0
+            while num>0:
+                r+= num%10
+                num//=10
+            mini= min(mini, r)
+        return mini
+
+
+solution_instance = Solution()
+
+result = solution_instance.minElement(nums = [10,12,13,14])
+print(result)
+
+result = solution_instance.minElement(nums = [1,2,3,4])
+print(result)
+
+result = solution_instance.minElement(nums = [999,19,199])
+print(result)
