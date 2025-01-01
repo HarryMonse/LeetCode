3fce52edb4130a46080f02bfbae7f3ea6bfa06f8
Harry Monse <harry.monse1812@gmail.com>
2025-01-01 20:10:25 +0530

    746

---
+++ 746.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(len(cost)-3,-1,-1):
+            cost[i]=min(cost[i]+cost[i+1],cost[i]+cost[i+2])
+        return min(cost[0],cost[1])
+
+
+solution_instance = Solution()
+
+result = solution_instance.minCostClimbingStairs(cost = [10,15,20])
+print(result)
+
+result = solution_instance.minCostClimbingStairs(cost = [1,100,1,1,1,100,1,1,100,1])
+print(result)
