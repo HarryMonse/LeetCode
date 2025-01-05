fe3c5ae5b7f492a06ce97eb6c0c34d43b19a96df
Harry Monse <harry.monse1812@gmail.com>
2025-01-05 17:03:28 +0530

    171

---
+++ 171.py
@@ -0,0 +1,18 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        result = 0
+        for char in columnTitle:
+            result = result * 26 + (ord(char) - ord('A') + 1)
+        return result
+
+
+solution_instance = Solution()
+
+result = solution_instance.titleToNumber(columnTitle = "A")
+print(result)
+
+result = solution_instance.titleToNumber(columnTitle = "AB")
+print(result)
+
+result = solution_instance.titleToNumber(columnTitle = "ZY")
+print(result)
